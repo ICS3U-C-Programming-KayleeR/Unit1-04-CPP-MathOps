257c27549898c7068b605b2a9becb19e03093cff
Kaylee Ralejoe <lintle.ralejoe@stu.ocsb.ca>
2026-09-18 01:44:58 +0000

    Added to github cloud

---
+++ basicMath.cpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2026 Kaylee R All rights reserved.
+// .
+// Created by: Kaylee R
+// Date: September 16 2026
+// This program performs basic math operations
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    std::cout << "5+7 = " << 5+7 << "\n";  // addition
+    std::cout << "56-32 = " << 56-32 << "\n";  // subtraction
+    std::cout << "4*31 = " << 4*31 << "\n";  // multiplication
+    std::cout << "567.0/9 = " << 567.0/9 << "\n";  // integer division
+    std::cout << "128%7 = " << 128%7 << "\n";  // decimal division
+    std::cout << "67^2 = " << pow(67, 2) << "\n";  // exponents
+    std::cout << "√76 = " << sqrt(76) << "\n";  // square root
+}
+
